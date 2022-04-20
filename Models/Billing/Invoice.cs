9ccb74b9bcb5712a5ed0d76529c89b2e9cde485d
salih <salih.karakaya@codefirst.io>
2022-04-21 00:52:36 +0300

    remove amount field, since it comes from invoiceType

--- Models/Billing/Invoice.cs
+++ Models/Billing/Invoice.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿
 
 namespace Apartment_Management.Models
 {
@@ -10,11 +9,6 @@ namespace Apartment_Management.Models
         public int InvoiceTypeID { get; set; }
         public int FlatID { get; set; }
 
-        [Required]
-        [Range(1, double.MaxValue)]
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Amount { get; set; }
-        
         public Period Period { get; set; }
         public InvoiceType InvoiceType { get; set; }
         public Flat Flat { get; set; }
