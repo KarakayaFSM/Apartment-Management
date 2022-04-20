4430853690a644567ed03863741ce998cf9e546c
salih <salih.karakaya@codefirst.io>
2022-04-20 15:34:11 +0300

    added required annotations to fields

--- Models/BillType.cs
+++ Models/BillType.cs
@@ -1,9 +1,13 @@
-﻿namespace Apartment_Management.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apartment_Management.Models
 {
     public class BillType
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public decimal Charge { get; set; }
     }
 }
--- Models/Period.cs
+++ Models/Period.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Apartment_Management.Models
 {
     public class Period
     {
+        [Required]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public DateTime PeriodDate { get; set; }
     }
 }
