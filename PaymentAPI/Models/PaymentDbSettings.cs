c7b2a56d46c86e502d2ff50c64c6f44cb6e0556e
salih <salih.karakaya@codefirst.io>
2022-04-21 17:45:28 +0300

    separate payment api

--- PaymentAPI/Models/PaymentDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaymentAPI.Models
-{
-    public class PaymentDbSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string BankCardsCollectionName { get; set; } = null!;
-    }
-}
