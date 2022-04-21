c6ea1ca63f6d6c22c150a00c9e1d75fd08f28700
salih <salih.karakaya@codefirst.io>
2022-04-21 17:56:27 +0300

    separate APIs

---
+++ PaymentAPI/Models/Account.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class Account
+    {
+        public string CardHolderName { get; set; }
+
+        [Required]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CardNumber { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public int ExpireYear { get; set; }
+
+        [Required]
+        public int ExpireMonth { get; set; }
+
+        [Required]
+        public int CVV { get; set; }
+        [Required]
+        public decimal Balance { get; set; } = 10000;
+    }
+}
---
+++ PaymentAPI/Models/PaymentDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentAPI.Models
+{
+    public class PaymentDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BankCardsCollectionName { get; set; } = null!;
+    }
+}
